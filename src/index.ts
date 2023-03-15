d2f01ba4a1567ceaf37da6d433dc98c9f2129c3a
Runtus <893119806@qq.com>
2023-03-15 21:08:44 +0800

    fix: 修复require引起的bug

--- src/index.ts
+++ src/index.ts
@@ -24,19 +24,19 @@ export interface Config {
 }
 
 export const Config: Schema<Config> = Schema.object({
-  refresh: Schema.string().required().description("Pixiv的RefreshToken"),
+  refresh: Schema.string().required().default("").description("Pixiv的RefreshToken"),
   keywords: Schema.object({
-    start: Schema.string().default("rpixiv酱").description("机器人的触发词。"),
-    day: Schema.string().default("查询每日推荐榜").description("每日推荐榜的触发语，紧跟着start字段触发词"),
-    week: Schema.string().default("查询每周推荐榜").description("每周推荐榜的触发语，紧跟着start字段触发词"),
-    month: Schema.string().default("查询每月推荐榜").description("每月推荐榜的触发语，紧跟着start字段触发词"),
-    searchIllusts: Schema.string().default("查询作品").description("输入关键字，获取关键字相关插画"),
-    searchAuthor:  Schema.string().default("查询作者").description("输入作者pid号，获取作者相关信息"),
+    start: Schema.string().default("").description("机器人的触发词。"),
+    day: Schema.string().default("").description("每日推荐榜的触发语，紧跟着start字段触发词"),
+    week: Schema.string().default("").description("每周推荐榜的触发语，紧跟着start字段触发词"),
+    month: Schema.string().default("").description("每月推荐榜的触发语，紧跟着start字段触发词"),
+    searchIllusts: Schema.string().default("").description("输入关键字，获取关键字相关插画"),
+    searchAuthor:  Schema.string().default("").description("输入作者pid号，获取作者相关信息"),
   }),
   proxy: Schema.object({
-    isOpen: Schema.boolean().default(true).description("是否开启代理"),
-    host: Schema.string().default("127.0.0.1").description("代理的host"),
-    port: Schema.number().default(7890).description("代理端口")
+    isOpen: Schema.boolean().default(false).description("是否开启代理"),
+    host: Schema.string().default("").description("代理的host"),
+    port: Schema.number().default(0).description("代理端口")
   })
 })
 
